cc4ed585d850eda82230ce1dce3253a978c62671
iremtektas <tektasirem13@gmail.com>
2025-05-03 18:10:42 +0300

    first commit

---
+++ k.p.s.s_mobile/k.p.s.s_mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  k.p.s.s_mobile
+//
+//  Created by iremt on 3.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
